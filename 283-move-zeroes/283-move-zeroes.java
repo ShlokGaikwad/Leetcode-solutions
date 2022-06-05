eaeeaa2992e862782f883e2a35e5c2833cdf1210
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2022-06-05 13:28:52 +0530

    Time: 3 ms (29.53%), Space: 54.5 MB (60.72%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count++;
+            }if(count!=0 && nums[i]!=0){
+                nums[i-count]=nums[i];
+                nums[i]=0;
+            }
+        }
+    }
+}
