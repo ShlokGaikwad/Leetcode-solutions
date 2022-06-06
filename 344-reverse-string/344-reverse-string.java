682e55458722f9ebb5b580199c2ed00110c2616d
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2022-06-06 09:21:33 +0530

    Time: 1 ms (97.37%), Space: 57.9 MB (18.16%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -3,7 +3,7 @@ class Solution {
         int i=0;
         int j=s.length-1;
         
-        while(i<=j){
+        while(i<j){
             char temp=s[i];
             s[i]=s[j];
             s[j]=temp;
