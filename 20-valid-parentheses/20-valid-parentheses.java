0c1a8b26aade2e78bcbdaa1432d8867e943d2425
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2022-06-17 15:04:10 +0530

    Time: 2 ms (87.37%), Space: 42.1 MB (47.28%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else if(st.isEmpty()){
+                return false;
+            }
+            else if(ch==')' && st.pop()!='('){
+                return false;
+            }
+            else if(ch==']' && st.pop()!='['){
+                return false;
+            }
+            else if(ch=='}' && st.pop()!='{'){
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
