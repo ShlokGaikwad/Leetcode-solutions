347669b462b42daef5a3c93c62e88ef48ce0abc0
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2022-06-17 14:19:01 +0530

    Time: 0 ms (100.00%), Space: 42 MB (36.47%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,25 +1,24 @@
 class MyStack {
 
-    public Queue<Integer> q = new LinkedList<>();
-    
+    public Queue<Integer> q=new LinkedList<>();
     public void push(int x) {
         q.add(x);
-        for(int i=1;i<q.size();i++)
+        for(int i=1;i<q.size();i++){
             q.add(q.remove());
+        }
     }
     
     public int pop() {
-       return q.remove();
-        
+        return q.remove();
     }
     
     public int top() {
         return q.peek();
+        
     }
     
     public boolean empty() {
-        return q.isEmpty();
-        
+        return q.isEmpty();    
     }
 }
 
