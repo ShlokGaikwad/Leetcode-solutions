4f66e9cd7d46d2a4635627a2e200e23a6e4d7f34
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2022-09-25 15:58:32 +0530

    Time: 6 ms (79.28%), Space: 96.3 MB (84.75%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,21 +10,38 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack <Integer> s = new Stack<>();
-        
-        ListNode temp=head;
-        while(temp!=null){
-            s.push(temp.val);
-            temp=temp.next;
-        }
-        temp=head;
-        while(temp!=null){
-            if(s.pop() != temp.val)
-                return false;
+        ListNode fast =head;
+        ListNode slow= head;
+       
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
             
-            temp=temp.next;
+        }
+        if(fast!=null){
+            slow=slow.next;
+        }
+        slow=reverse(slow);
+        fast=head;
+        
+        while(slow!=null){
+            if(slow.val != fast.val){
+                return false;
+            }
+            fast=fast.next;
+            slow=slow.next;
         }
         return true;
-        
+     
+    }
+    public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        while(head!=null){
+            ListNode next=head.next;
+            head.next=prev;
+            prev=head;
+            head=next;
+        }
+        return prev;
     }
 }
