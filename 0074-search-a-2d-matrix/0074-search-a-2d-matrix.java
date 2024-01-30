414c710b08946821c63db93504a271fe583877ff
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-01-30 17:54:47 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (70.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n=matrix.length,m=matrix[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(target==matrix[i][j]) return true;
-            }
-        }
+        int i=0,j=matrix[0].length-1;
+       while(i<matrix.length && j>=0){
+           if(matrix[i][j]==target) return true;
+           else if(matrix[i][j]>target) j--;
+           else i++;
+       }
         return false;
     }
 }
