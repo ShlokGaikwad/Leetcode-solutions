d5124af88ab4a1bac850b70a2c843bf1fb9f3e26
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-02-05 22:19:45 +0530

    Time: 165 ms (87.10%), Space: 55.2 MB (75.81%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,39 +1,55 @@
 class Solution {
-    static long INF = (long) 1e10;
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        int m = nums1.length, n = nums2.length;
-        long lo = -INF - 1, hi = INF + 1;
-        while (lo < hi) {            
-            long mid = lo + ((hi - lo) >> 1), cnt = 0;
-            for (int i : nums1) {
-                if (0 <= i) {
-                    int l = 0, r = n - 1, p = 0;
-                    while (l <= r) {
-                        int c = l + ((r - l) >> 1);
-                        long mul = i * (long) nums2[c];
-                        if (mul <= mid) {
-                            p = c + 1;
-                            l = c + 1;
-                        } else r = c - 1;
-                    }
-                    cnt += p;
-                } else {
-                    int l = 0, r = n - 1, p = 0;
-                    while (l <= r) {
-                        int c = l + ((r - l) >> 1);
-                        long mul = i * (long) nums2[c];
-                        if (mul <= mid) {
-                            p = n - c;
-                            r = c - 1;
-                        } else l = c + 1;
-                    }
-                    cnt += p;
-                }
+        
+        List<Integer> p1=new ArrayList<>(),
+        n1=new ArrayList<>(),
+        p2=new ArrayList<>(),
+        n2=new ArrayList<>();
+        
+        for (int num : nums1) {
+            if (num<0) {
+                n1.add(num);
+            } else {
+                p1.add(num);
             }
-            if (cnt >= k) {
-                hi = mid;
-            } else lo = mid + 1L;
         }
-        return lo;
+        for (int num : nums2) {
+            if (num < 0) {
+                n2.add(num);
+            } else {
+                p2.add(num);
+            }
+        }
+
+        long left = -(long)1e10, right = (long)1e10;
+        while (left < right ){
+            long mid = (right + left) >> 1;
+            //System.out.println("right: "+right+" mid: "+mid);
+            //cur, <=mid
+            long cur=0;
+            //p1*n2
+            for(int i=0, j=0;i<p1.size();i++){
+                while(j<n2.size() && (long)p1.get(i)*n2.get(j)<=mid) j++;
+                cur+=j;
+            }
+            //n1*p2
+            for(int i=0, j=0;i<n1.size();i++){
+                while(j<p2.size() && (long)n1.get(i)*p2.get(j)>mid) j++;
+                cur+=(p2.size()-j);
+            }
+            //p1*p2
+            for(int i=0, j=p2.size()-1;i<p1.size();i++){
+                while(j>=0 && (long)p1.get(i)*p2.get(j)>mid) j--;
+                cur+=(j+1);
+            }
+            //n1*n2
+            for(int i=0, j=n2.size()-1;i<n1.size();i++){
+                while(j>=0 && (long)n1.get(i)*n2.get(j)<=mid) j--;
+                cur+=n2.size()-j-1;
+            }
+            if(cur<k) left=mid+1;
+            else right=mid;
+        }
+        return left;
     }
 }
