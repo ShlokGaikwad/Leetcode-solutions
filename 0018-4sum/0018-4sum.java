57862cea749e4d65daab9d4767729d3e5e556251
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-02-05 22:10:10 +0530

    Time: 17 ms (61.69%), Space: 44 MB (55.61%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,40 +3,29 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         List<List<Integer>> result = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
             for (int j = i + 1; j < n; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
-
                 int start = j + 1;
                 int end = n - 1;
-
                 while (start < end) {
-
                     long sum = (long) nums[i] + nums[j] + nums[start] + nums[end];
-
                     if (sum == target) {
-
                         result.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
-
                         start++;
                         end--;
-
                         while (start < end && nums[start] == nums[start - 1]) {
                             start++;
                         }
-
                         while (start < end && nums[end] == nums[end + 1]) {
                             end--;
                         }
                     } else if (sum < target) {
-
                         start++;
                     } else {
                         end--;
