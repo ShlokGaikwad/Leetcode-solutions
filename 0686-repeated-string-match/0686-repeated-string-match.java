776ab2ca9badf93b191a08c72c1e330e0bb92a9c
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-02-07 23:25:25 +0530

    Time: 2 ms (100.00%), Space: 42.3 MB (59.44%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,24 +1,19 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        StringBuilder s = new StringBuilder(a);
-        int count = 1;
-
-        while (s.length() < b.length()) {
-            s.append(a);
-            count++;
+        int n = a.length();
+        int m = b.length();
+        boolean[] bucket = new boolean[26];
+        for (char c : a.toCharArray()) bucket[c - 'a'] = true;
+        for (char c : b.toCharArray()) 
+            if (!bucket[c - 'a']) return -1;
+        StringBuilder sb = new StringBuilder();
+        int factor = m/n + (m%n>0? 1:0);
+        for(int i=0; i<factor; i++){
+            sb.append(a);
         }
-
-        if (s.indexOf(b) != -1) {
-            return count;
-        }
-
-        s.append(a);
-        count++;
-
-        if (s.indexOf(b) != -1) {
-            return count;
-        }
-
+        if(sb.indexOf(b)>=0) return factor;
+        sb.append(a);
+        if(sb.indexOf(b)>=0) return factor+1;
         return -1;
     }
 }
