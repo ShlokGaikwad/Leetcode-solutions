33e4e99489c46b95523de7d69a8d7818445956ba
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-02-18 00:24:24 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (83.70%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-       int dp[]=new int[n+1];
+//         if( n<=1) return 1;
+//         if(n==2) return 2;
+
+        
+//         return climbStairs(n-1)+climbStairs(n-2);
+        
+        int dp[]=new int[n+1];
         if(n<=1) return 1;
         dp[1]=1;
         dp[2]=2;
@@ -8,6 +14,5 @@ class Solution {
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-        
     }
 }
