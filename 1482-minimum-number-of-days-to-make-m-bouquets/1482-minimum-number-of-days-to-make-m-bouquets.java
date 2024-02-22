356c504a03107c1d4df6a19743fbd91f11cce143
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-02-22 15:43:29 +0530

    Time: 17 ms (84.16%), Space: 58.7 MB (10.87%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -2,39 +2,37 @@ class Solution {
     public int minDays(int[] arr, int m, int k) {
         return min(arr,m,k);
     }
-    public int min(int arr[],int m,int k){
-        long val=(long)m*k;
-        if(val>arr.length) return -1;
-        
-        int low=Integer.MAX_VALUE,high=Integer.MIN_VALUE;
-        int ans=0;
-        for(int i=0;i<arr.length;i++){
-            low=Math.min(arr[i],low);
-            high=Math.max(arr[i],high);
-        }
-            while(low<=high){
-                int mid=(high+low)/2;
-                if(possible(arr,mid,m,k)){
-                    ans=mid;
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }
-            }
-        
-        return ans;
-    }
-    public boolean possible(int arr[],int time,int m,int k ){
-        int count=0,NoD=0;
+   public static int min(int arr[],int m,int k){
+       long val=(long)m*k;
+       if(val>arr.length) return -1;
+       int low=Integer.MAX_VALUE,high=Integer.MIN_VALUE;
+       for(int i=0;i<arr.length;i++){
+           low=Math.min(arr[i],low);
+           high=Math.max(arr[i],high);
+       }
+       int ans=0;
+       while(low<=high){
+           int mid=(high+low)/2;
+           if(possible(arr,mid,m,k)){
+               ans=mid;
+               high=mid-1;
+           }else{
+               low=mid+1;
+           }
+       }
+       return ans;
+   }
+    public static boolean possible(int arr[],int time,int m,int k){
+        int count=0,deck=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]<=time){
                 count++;
             }else{
-                NoD+=(count/k);
+                deck+=(count/k);
                 count=0;
             }
         }
-        NoD+=(count/k);
-        return NoD>=m;
+        deck+=(count/k);
+        return deck>=m;
     }
 }
