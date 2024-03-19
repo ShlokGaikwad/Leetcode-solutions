af9150cd0c37794e1ef93180f8d01bd8bc88783d
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-03-19 11:51:21 +0530

    Time: 25 ms (76.09%), Space: 40.4 MB (38.61%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,16 +3,15 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int low=0,high=n;
-        
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(isBadVersion(mid)==true && isBadVersion(mid-1)==false) return mid;
-            else if(isBadVersion(mid)==false) low=mid+1;
-            else high=mid;
+       int s=1 ,e=n;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(isBadVersion(mid)){
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
         }
-        return -1;
+        return s;
     }
-    
-    
 }
