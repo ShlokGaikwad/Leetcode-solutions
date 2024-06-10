c4654e132f7d9419f1d3b50f63f5d68c900c4b21
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-06-10 14:08:26 +0530

    Time: 3 ms (73.02%), Space: 45 MB (28.45%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.java
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPoisonedDuration(int[] arr, int k) {
-       int sum=0;
+        int sum=0;
         for(int i=0;i<arr.length-1;i++){
             if(arr[i+1]<=arr[i]+k){
                 sum+=arr[i+1]-arr[i];
