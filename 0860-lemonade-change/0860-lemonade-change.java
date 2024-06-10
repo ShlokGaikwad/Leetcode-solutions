2db02fbacdc01f6539a86ff49e1f6d3f6d7504fb
Shlok gaikwad <99670746+ShlokGaikwad@users.noreply.github.com>
2024-06-10 14:32:41 +0530

    Time: 2 ms (95.07%), Space: 55.5 MB (78.00%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -2,21 +2,21 @@ class Solution {
     public boolean lemonadeChange(int[] arr) {
         int five=0,ten=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==5 ){
+            if(arr[i]==5){
                 five++;
-            }else if(arr[i]==10 ){
-                five--;
+            }else if(arr[i]==10){
                 ten++;
+                five--;
             }else if(ten>0){
-                   ten--;
-                   five--;
-               }else{
-                   five-=3;
-               }
-                if(five<0){
-                    return false;
-                }
+                ten--;
+                five--;
+            }else{
+                five-=3;
+            }
+            if(five<0) return false;
         }
-        return true;
+    return true;
     }
+    
+    
 }
